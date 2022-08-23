4fba0bc3ee4d4aa37a0c1d6cc7d36429205eb4f0
natnaelasgedom <natnael.asgedom@gmail.com>
2022-08-23 20:49:38 +0200

    Created screens and structure

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './app/screens/HomeScreen';
 
 //Hei, Gael! Dette var en fin setning!
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen/>
     </View>
   );
 }
---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import colors from '../config/colors';
+
+function HomeScreen(props) {
+    return (
+        <View style={styles.background}>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: colors.primary,
+        flex: 1
+    }
+})
+
+export default HomeScreen;
