71f07402882f3363d72b679f695b1b86d82e7f07
natnaelasgedom <natnael.asgedom@gmail.com>
2022-08-23 22:23:19 +0200

    Removed comments from App.js

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './app/screens/HomeScreen';
 
-//Hei, Gael! Dette var en fin setning!
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen/>
+      <Text>Hello, Miriam</Text>
     </View>
   );
 }
-
+// <HomeScreen/>
 const styles = StyleSheet.create({
   container: {
     flex: 1,
