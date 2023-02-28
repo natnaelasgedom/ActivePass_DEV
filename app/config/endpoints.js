5513560a4260617ffa74eab7ebebf65f9a4c6b05
Natnael Asgedom <natnael.asgedom@rema.no>
2023-02-28 10:51:09 +0100

    update endpoints

--- app/config/endpoints.js
+++ app/config/endpoints.js
@@ -1,19 +1,4 @@
 export default {
-<<<<<<< HEAD
-    pokemon: {
-        baseUrl: 'https://pokeapi.co/api/v2/pokemon/'
-    },
-    jsonPlaceholder: {
-        baseUrl: 'https://jsonplaceholder.typicode.com/',
-        users: 'https://jsonplaceholder.typicode.com/users', //10 users,
-        todos: 'https://jsonplaceholder.typicode.com/todos', //200 todos
-        posts: 'https://jsonplaceholder.typicode.com/posts' //100 posts
-    },
-    activePassAPI: {
-        baseUrl: 'https://activepassapi20230222221930.azurewebsites.net/api/'
-    }
-}
-=======
   pokemon: {
     baseUrl: "https://pokeapi.co/api/v2/pokemon/",
   },
@@ -29,4 +14,3 @@ export default {
     postman: "https://5fcda094-e4b8-48dc-be5a-604cd91665f4.mock.pstmn.io/",
   },
 };
->>>>>>> 019e9f18d9eb66535773dcf5865b35014436838b
